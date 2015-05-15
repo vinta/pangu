89e99d494a914142a13d9331f3e4e5f44123ae11
Vinta <vinta.chen@gmail.com>
2015-05-16 02:02:51 +0800

    add BenchmarkTextSpacing()

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,8 +1,15 @@
 package pangu_test
 
 import (
+	"github.com/vinta/pangu"
 	"testing"
 )
+
+func BenchmarkTextSpacing(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		pangu.TextSpacing("所以,請問Jackey的鼻子有幾個?3.14個!")
+	}
+}
 
 func BenchmarkFileSpacing(b *testing.B) {
 	for i := 0; i < b.N; i++ {
