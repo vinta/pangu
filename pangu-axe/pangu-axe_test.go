13ba8ec2968d86ba4014b61b3cba7f2112aeacc4
Vinta <vinta.chen@gmail.com>
2015-05-17 02:03:17 +0800

    experiment

--- pangu-axe/pangu-axe_test.go
+++ pangu-axe/pangu-axe_test.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
 func TestText(t *testing.T) {
+	realStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
 	os.Args = []string{NAME, "text", "新八的構造成分有95%是眼鏡、3%是水、2%是垃圾"}
 	main()
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = realStdout
+	fmt.Printf("%s", out)
 }
